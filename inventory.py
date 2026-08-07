c97afada4a1ec4e1b79f3f8030c2497929267863
bnhenry08 <bnhenry08@users.noreply.github.com>
2026-08-07 10:02:07 -0400

    updated home

--- inventory.py
+++ inventory.py
@@ -9,6 +9,19 @@ st.title("🧬 Laboratory Inventory")
 
 st.write("Select the freezer inventory you would like to open.")
 
-st.info(
-    "Use the sidebar on the left to select the freezer inventory."
-)
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button(
+        "❄️ -20°C Inventory",
+        use_container_width=True
+    ):
+        st.switch_page("pages/inventory_20C.py")
+
+
+with col2:
+    if st.button(
+        "🧊 -80°C Inventory",
+        use_container_width=True
+    ):
+        st.switch_page("pages/inventory_80C.py")
